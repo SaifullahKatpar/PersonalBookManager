f48464d93ce1502028b4557dae3ec0e705919fe3
Saifullah <saifullah.cs14@iba-suk.edu.pk>
2019-02-17 00:41:52 +0500

    removed irrelvant code

--- catalog/templates/catalog/filters.py
+++
@@ -1,8 +0,0 @@
-from .models import Book
-import django_filters
-
-class BookFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains')
-    class Meta:
-        model = Book
-        fields = ['title']
