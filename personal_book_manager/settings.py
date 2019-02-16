620015d531473c09a125b3f137145a3ca45727c0
Saifullah <saifullah.cs14@iba-suk.edu.pk>
2019-02-16 13:25:15 +0500

    modified some settings

--- personal_book_manager/settings.py
+++ personal_book_manager/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%**)1+-av0!1tmur#auz52blrm(=a+8_8fk&u+9otbpt_no-tm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*.herokuapps.com','localhost']
 
 
 # Application definition
